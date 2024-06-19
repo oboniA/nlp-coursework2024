656f8c9e6b3ab349abcf6cd8e0bc0aa19ef8a86c
Oboni <oanowe01@student.bbk.ac.uk>
2024-06-19 15:35:09 +0100

    Part 1 (c) return dictionary  of novel title with Flesch-Kincaid Grade level scores

--- PartOne.py
+++ PartOne.py
@@ -2,6 +2,8 @@ import nltk
 import spacy
 from pathlib import Path
 import pandas as pd
+from nltk.tokenize import word_tokenize, sent_tokenize
+import re
 
 
 nlp = spacy.load("en_core_web_sm")
@@ -20,7 +22,25 @@ def fk_level(text, d):
     Returns:
         float: The Flesch-Kincaid Grade Level of the text. (higher grade is more difficult)
     """
-    pass
+    words = word_tokenize(text)
+    sentence = sent_tokenize(text)
+    syllables = sum(count_syl(word, d) for word in words)
+    word_nums = len(words)
+    sent_nums = len(sentence)
+
+    # prevents zero division errors
+    if (word_nums == 0) or (sent_nums == 0):
+        fk_grade_level = 0  
+    else:
+        avg_sent_len = word_nums / sent_nums
+        avg_syl_per_word = syllables / word_nums
+
+        # using FK Grade Level formula
+        fk_grade_level = 0.39 * avg_sent_len + 11.8 * avg_syl_per_word - 15.59
+    
+    return fk_grade_level
+
+
 
 
 def count_syl(word, d):
@@ -34,7 +54,29 @@ def count_syl(word, d):
     Returns:
         int: The number of syllables in the word.
     """
-    pass
+    word = word.lower() # lower-case for case-ensivitiy 
+
+    # checks word existence in CMU dict
+    if word in d:
+        max_syl=0
+        # itirates over word pronunciations in dict
+        for pronunciation in d[word]:
+            syl_count=0
+            # when last char of a phone is digit
+            # increment syl count for that pronunciation
+            for phone in pronunciation:
+                if phone[-1].isdigit():  
+                    syl_count += 1  
+            # update max syl count if current pronunciation have more syls
+            if syl_count > max_syl:
+                max_syl = syl_count
+
+        return max_syl
+    
+    else:
+        # if word not in dict, estimate by counting vowels
+        return len(re.findall(r'[aeiou]+', word))
+
 
 
 def read_novels(path=Path.cwd() / "texts" / "novels"):
@@ -80,22 +122,28 @@ def parse(df, store_path=Path.cwd() / "texts" / "novels" / "parsed", out_name="p
 
 def regex_ttr(text):
     """Calculates the type-token ratio of a text. Text is tokenized using a regular expression."""
-
+    
     pass
 
 
 def nltk_ttr(text):
     """Calculates the type-token ratio of a text. Text is tokenized using nltk.word_tokenize."""
 
-    pass
+    # tokenize text with nltk lib
+    tokens = word_tokenize(text)
+    token_types = set(tokens)
+    token_type_ratio= len(token_types) / len(tokens)
+
+    return(token_type_ratio)
+
 
 
 def get_ttrs(df):
     """helper function to add ttr to a dataframe"""
     results = {}
     for i, row in df.iterrows():
-        results[row["title"]] = regex_ttr(row["text"])
-    return results
+        results[row["title"]] = nltk_ttr(row["text"])
+    return f"\nToken Type Ratio: {results}"
 
 
 def get_fks(df):
@@ -104,7 +152,7 @@ def get_fks(df):
     cmudict = nltk.corpus.cmudict.dict()
     for i, row in df.iterrows():
         results[row["title"]] = round(fk_level(row["text"], cmudict), 4)
-    return results
+    return f"\nFlech-Kincaid Grade level scores: {results}"
 
 
 def subjects_by_verb_pmi(doc, target_verb):
@@ -137,15 +185,16 @@ if __name__ == "__main__":
     path = Path.cwd() / "texts" / "novels"
     print(path)
     df = read_novels(path) # this line will fail until you have completed the read_novels function above.
-    print(df)
-    #print(df.head())
-    #nltk.download("cmudict")
+    print(df.head(10))
+    nltk.download("cmudict")
     #parse(df)
     #print(df.head())
-    #print(get_ttrs(df))
-    #print(get_fks(df))
+    print(get_ttrs(df))
+    print(get_fks(df))
     #df = pd.read_pickle(Path.cwd() / "texts" / "novels" / "parsed" / "name.pickle")
     # print(get_subjects(df))
+
+
     """ 
     for i, row in df.iterrows():
         print(row["title"])
